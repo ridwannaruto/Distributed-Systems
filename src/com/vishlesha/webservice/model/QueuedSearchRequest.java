6a02e8718d672c211778128d5dc88718db5e17fd
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2016-02-10 19:50:08 +0530

    Adding queued search requests models

---
+++ src/com/vishlesha/webservice/model/QueuedSearchRequest.java
@@ -0,0 +1,81 @@
+package com.vishlesha.webservice.model;
+
+import com.vishlesha.app.GlobalState;
+
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Vector;
+
+/**
+ * Created by tharindu on 2/9/16.
+ */
+public class QueuedSearchRequest {
+
+	private String initiator = "";
+	private String query = "SER";
+
+	private Vector<String> out=new Vector<String>();
+	private String in="";
+
+	private int index=-1;
+
+	public QueuedSearchRequest() {
+
+	}
+
+	public void setIndex(final int i) {
+		new java.util.Timer().schedule(
+				new java.util.TimerTask() {
+					@Override
+					public void run() {
+						SearchContext.processingRequest.remove(i);
+					}
+				},
+				5000
+		);
+	}
+
+	public String getInitiator() {
+		return initiator;
+	}
+
+
+	public void setInitiator(String initiator) {
+		this.initiator = initiator;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public Vector<String> getOut() {
+		return out;
+	}
+
+	public void setOut(Vector<String> out) {
+		this.out = out;
+	}
+
+	public String getIn() {
+		return in;
+	}
+
+	public void setIn(String in) {
+		this.in = in;
+	}
+
+
+	@Override public boolean equals(Object other) {
+		boolean result = false;
+		if (other instanceof QueuedSearchRequest) {
+			QueuedSearchRequest that = (QueuedSearchRequest) other;
+			result = (this.getInitiator().equals(that.getInitiator()) && this.getQuery().equals(that.getQuery()));
+		}
+		return result;
+	}
+
+
+}
